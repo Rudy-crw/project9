08680d1e22367d5a5e0a266c7373cd3b3db327ae
Rudy-crw <rudy.wu@outlook.com>
2025-06-26 00:51:46 +0800

    update for server push Render

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 
 //連結MongoDB
 mongoose
-  .connect("mongodb://127.0.0.1/mernDB")
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("連結到mongodb....");
   })
@@ -22,7 +22,15 @@ mongoose
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://project9-client-nw89.onrender.com/", // 你的前端 Render 網址
+    ],
+    credentials: true,
+  })
+);
 
 app.use("/api/user", authRoute);
 //course route應該被jwt保護
@@ -35,6 +43,10 @@ app.use(
 //只有登入系統的人(手上有JWT)，才能新增課程或註冊課程
 
 //因為port3000是React預設的
-app.listen(8080, () => {
-  console.log("後端伺服器聆聽在port 8080...");
+// app.listen(8080, () => {
+//   console.log("後端伺服器聆聽在port 8080...");
+// });
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`後端伺服器聆聽在port ${PORT}...`);
 });
